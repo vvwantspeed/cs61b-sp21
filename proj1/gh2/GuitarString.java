6809cc2ef3c710ab511fbf16d7a2fe2a2f2f2630
volderoth <volderoth@163.com>
2021-03-05 16:17:27 +0800

    proj1 GuitarString style fix

--- proj1/gh2/GuitarString.java
+++ proj1/gh2/GuitarString.java
@@ -12,7 +12,7 @@ public class GuitarString {
     private static final double DECAY = .996; // energy decay factor
 
     /* Buffer for storing sound data. */
-     private Deque<Double> buffer;
+    private Deque<Double> buffer;
 
     /* Create a guitar string of the given frequency.  */
     public GuitarString(double frequency) {
